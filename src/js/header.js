04c1dd5d2997d56b425acc6093344a4d9bb28c71
Karina <karinaa121214@gmail.com>
2020-09-08 13:48:39 +0200

    added sections: follow, footer, products-section, quotes; updated styles

---
+++ src/js/header.js
@@ -0,0 +1,12 @@
+export function scrolledHeader() {
+    let headerNav = document.getElementById('header')
+    window.onscroll = function () {
+        let scrolled = window.pageXOffset || document.documentElement.scrollTop;
+        if (scrolled > 60) {
+            headerNav.style.backgroundColor = "black"
+        } else {
+            headerNav.style.backgroundColor = "transparent"
+        }
+    }
+}
+scrolledHeader()
