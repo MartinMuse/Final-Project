716fee7feed8c579936fe0253abe177f127df6d9
Karina <karinaa121214@gmail.com>
2020-09-14 20:15:51 +0200

    added Product class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // import {scrolledHeader} from "./js/header.js";
 // import {Comment} from './js/comments.js'
 
-
 class API {
     constructor(category, key) {
         this.category = category;
         this.key = key || '5f560e6a32f56200168bcdc3';
+        this.data = [];
     }
 
     createUrl() {
@@ -13,78 +13,109 @@ class API {
         return this;
     }
 
-     async getResponse (){
-            const response = await fetch(this.appUrl, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8'
-                }
-            });
+    get f() {
+        return this.data
+    }
 
-            let result = await response.json();
-            if (!response.ok) {                                                      //если ответ не 200
-                throw new Error(`Error, status: ${response.status}`);
-            } else {
-                 renderProduct(result)
+    async getResponse() {
+        const response = await fetch(this.appUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
             }
-         return result
+        });
+
+        const result = await response.json();
+        if (!response.ok) {                                                      //если ответ не 200
+            throw new Error(`Error, status: ${response.status}`);
+        } else {
+            //Product.renderProduct(result)
+            Product.renderProduct(result)
+        }
+        return result
     }
 }
 
-const product = new API('products');
-product.createUrl().getResponse()
+const request = new API('products');
+request.createUrl().getResponse()
+request.f
 
 const productList = document.getElementById('productList')
 
-function renderProduct(data) {
-    if (data !== undefined) {
-        let  arr
+class Product {
+    constructor(category) {
+        this.category = category || 'Assortments';
+        // this.data = request.f
+    }
+
+    static checkCategory(data) {
+        let arr
+        data.forEach((item) => {
+            if (item.category === 'Assortments') {
+                return arr = item.product
+            }
+        })
+        return arr
+    }
+
+    static renderProduct(data) {
+        let arr
         data.forEach(function (item) {
             if (item.category == 'Assortments') {
                 return arr = item.product
             }
+            console.log('Arr', arr)
+            return arr
         })
 
         arr.forEach(function (item) {
             const HTML = `<li class="product__item" >
-                <div class="product__img-box">
-                    <a href="">
-                        <img class="product__img"
-                             src=${item.assortImage}>
-                    </a>
-                </div>
+            <div class="product__img-box">
+                <a href="">
+                    <img class="product__img"
+                         src=${item.assortImage}>
+                </a>
+            </div>
 
-                <div class="product__description">
-                    <h4 class="title--uppercase">
-                        <a class="product__title">${item.title}</a>
-                    </h4>
-                    <span class="product__description sub-title ">${item.description}</span>
-                    <span class="product__cost">${item.cost} </span>
-                    <button  data-id=${item.id} class="button to-cart categories__button--hovered product__button">
-                        <i class="fa fa-cart-arrow-down product__fa" aria-hidden="true"></i>
-                        to card
-                    </button>
-                </div>
-            </li>`;
+            <div class="product__description">
+                <h4 class="title--uppercase">
+                    <a class="product__title">${item.title}</a>
+                </h4>
+                <span class="product__description sub-title ">${item.description}</span>
+                <span class="product__cost">${item.cost} </span>
+                <button  data-id=${item.id} class="button to-cart categories__button--hovered product__button">
+                    <i class="fa fa-cart-arrow-down product__fa" aria-hidden="true"></i>
+                    to card
+                </button>
+            </div>
+        </li>`;
             productList.insertAdjacentHTML("afterbegin", HTML);
         })
+
+
+        // addListener() {
+        //     const cart = {};
+        //     productList.addEventListener('click',
+        //
+        //         function (e) {
+        //             const target = e.target;
+        //
+        //             if (target.classList.contains('to-cart')) {
+        //                 const id = target.dataset['id']
+        //
+        //                 if (cart.hasOwnProperty(id)) {
+        //                     cart[id][0] += 1
+        //                 } else {
+        //                     cart[id] = [1]
+        //                 }
+        //                 localStorage.setItem('cart', JSON.stringify(cart))
+        //                 console.log('LocalStorage', localStorage)
+        //             }
+        //         }
+        //     )
+        // }
     }
 }
 
-const cart = {};
-productList.addEventListener('click', function (e) {
-    const target = e.target;
-
-    if (target.classList.contains('to-cart')) {
-        const id = target.dataset['id']
-
-        if (cart .hasOwnProperty(id)) {
-            cart[id][0] += 1
-        } else {
-            cart[id] = [1]
-        }
-        localStorage.setItem('cart', JSON.stringify(cart))
-        console.log('LocalStorage', localStorage)
-    }
-})
-
+const product = new Product('Assortments')
+console.log(product)
