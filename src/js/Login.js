66f11c5821ae895164dc54aa973df7d22df0beb4
MartinMuse <alex.1gorokhovsky@icloud.com>
2020-09-17 19:09:11 +0300

    updated login class

--- src/js/Login.js
+++ src/js/Login.js
@@ -2,49 +2,49 @@ const url = "https://5f560e6a32f56200168bcdc3.mockapi.io/api/data/users";
 
 class LoginClass {
   constructor(passwordInputId = "input-password", emailInputId = "input-email", loginEnterId="loginEnter") {
-    this._loginBtn = document.getElementById(loginEnterId);
-    this._passwordInput = document.getElementById(passwordInputId);
-    this._emailInput = document.getElementById(emailInputId);
-    this._userData = {}
-    this._loginContainer=document.querySelector(".login__container")
-    this._messageContainer=document.querySelector(".message__container")
+    this.loginBtn = document.getElementById(loginEnterId);
+    this.passwordInput = document.getElementById(passwordInputId);
+    this.emailInput = document.getElementById(emailInputId);
+    this.userData = {}
+    this.loginContainer=document.querySelector(".login__container")
+    this.messageContainer=document.querySelector(".message__container")
 
-    this._serverResponse = async function () {
+    this.serverResponse = async function () {
       let response = await fetch(url)
       return await response.json()
     }
 
-    this._isCorrectUser = function (elem, index, array) {
-      if ((elem.password === this._userData.password) && (elem.email === this._userData.email))
-        this._userData.name = elem.name;
-      return (elem.password === this._userData.password) && (elem.email === this._userData.email)
+    this.isCorrectUser = function (elem, index, array) {
+      if ((elem.password === this.userData.password) && (elem.email === this.userData.email))
+        this.userData.name = elem.name;
+      return (elem.password === this.userData.password) && (elem.email === this.userData.email)
     }.bind(this)
   }
 
   setUserData(){
-    this._userData.password=this._passwordInput.value;
-    this._userData.email=this._emailInput.value;
+    this.userData.password=this.passwordInput.value;
+    this.userData.email=this.emailInput.value;
   }
 
   addListener() {
-    this._loginBtn.addEventListener('click', (e) => {
+    this.loginBtn.addEventListener('click', (e) => {
       e.preventDefault()
       this.loginEvent();
     })
   }
 
   hideLoginSection(){
-    this._loginContainer.hidden=true;
+    this.loginContainer.hidden=true;
   }
 
   createMessage(){
-    this._messageContainer.innerHTML="<span class='message__title'>Login successful!</span>"
+    this.messageContainer.innerHTML="<span class='message__title'>Login successful!</span>"
   }
 
   loginEvent = async function () {
-    const users = await this._serverResponse()
+    const users = await this.serverResponse()
     this.setUserData();
-    if (users.some(this._isCorrectUser)) {
+    if (users.some(this.isCorrectUser)) {
       localStorage.setItem('userStatus','true')//установка статуса
       this.hideLoginSection();
       this.createMessage();
